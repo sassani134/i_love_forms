8eca97d54a3b56faf4149a6d1a6431a4d57cbd7b
RomainRec <romain.recher@gmail.com>
2018-10-30 22:41:36 +0100

    code local ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   
-  get '/', to: 'users#create'
-  get 'new', to: 'users#new'
-  post 'new', to: 'users#new'
+  root 'users#index'
+  get 'for', to: 'users#for'
+  get 'tag', to: 'users#tag'
+  get 'create', to: 'users#create'
+  get '/create/new', to: 'users#new'
+  post '/create/new', to: 'users#new'
+  get '/tag/new', to: 'users#new'
+  post '/tag/new', to: 'users#new'
+  get '/for/new', to: 'users#new'
+  post '/for/new', to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
