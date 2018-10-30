a563c8d0c629f0154ea952da0a203b5ca7fce3da
RomainRec <romain.recher@gmail.com>
2018-10-30 21:23:10 +0100

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def new
+  	puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+  	puts params
+  	my_name_param = params[:username]
+  	puts my_name_param
+  	@user = User.new 
+ 	@user.username = params[:username]
+ 	@user.email = params[:email]
+	@user.bio = params[:bio]
+	@user.save 
   end
 
   def create
+
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/', to: 'users#new'
-  post '/', to: 'users#create'
+  
+  get '/', to: 'users#create'
+  get 'new', to: 'users#new'
+  post 'new', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
